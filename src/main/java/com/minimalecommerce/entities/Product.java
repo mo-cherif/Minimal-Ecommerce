69cc958380683c62b6a43c0551dee0d5c0de72c1
mo-cherif <mocherifh0@gmail.com>
2024-03-08 20:54:41 +0100

    feat: basic model

--- src/main/java/com/minimalecommerce/entities/Product.java
+++ src/main/java/com/minimalecommerce/entities/Product.java
@@ -1,4 +1,58 @@
 package com.minimalecommerce.entities;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String title;
+    private boolean status;
+
+    @ManyToOne
+    private Category category;
+
+    public Product(String name, String title, boolean status, Category category) {
+        this.name = name;
+        this.title = title;
+        this.status = status;
+        this.category = category;
+    }
+
+    public Product() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
