f8247f7eb93f78aaa83359688ca8f7fb0bc3a05a
mo-cherif <mocherifh0@gmail.com>
2024-03-09 22:12:51 +0100

    feat: added lombok pluggin

--- src/main/java/com/minimalecommerce/entities/Product.java
+++ src/main/java/com/minimalecommerce/entities/Product.java
@@ -1,8 +1,11 @@
 package com.minimalecommerce.entities;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Table(name = "products")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString @Builder
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,45 +17,4 @@ public class Product {
     @ManyToOne
     private Category category;
 
-    public Product(String name, String title, boolean status, Category category) {
-        this.name = name;
-        this.title = title;
-        this.status = status;
-        this.category = category;
-    }
-
-    public Product() {
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public boolean isStatus() {
-        return status;
-    }
-
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
 }
